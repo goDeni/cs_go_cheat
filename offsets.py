94567b1932b7839a7e756c21d9bdd672f1e59788
goDeni <den4ikept11@gmail.com>
2020-05-06 11:37:27 +0500

    Сделал чтение OFFSET-ов с файла из репозитория hazedumper

--- offsets.py
+++ offsets.py
@@ -1,11 +1,24 @@
-m_iHealth = 0x100
-dwEntityList = 0x4D43AB4
-dwViewMatrix = 0x4D353F4
-m_iTeamNum = 0xF4
-m_vecOrigin = 0x138
-m_lifeState = 0x25F
-dwLocalPlayer = 0xD2FB84
-m_Collision = 0x320
-is_c4_owner = 0x3A5890
-m_iCrosshairId = 0xB3D4
-dwForceAttack = 0x3175088
+import json
+from typing import Dict, Any
+
+with open('hazedumper/csgo.json', 'r') as f_:
+    CSGO_OFFSETS: Dict[str, Any] = json.load(f_)
+
+SIGNATURES: Dict[str, int] = CSGO_OFFSETS['signatures']
+NETVARS: Dict[str, int] = CSGO_OFFSETS['netvars']
+
+
+m_iHealth = NETVARS['m_iHealth']
+dwEntityList = SIGNATURES['dwEntityList']
+dwViewMatrix = SIGNATURES['dwViewMatrix']
+m_iTeamNum = NETVARS['m_iTeamNum']
+m_vecOrigin = NETVARS['m_vecOrigin']
+m_lifeState = NETVARS['m_lifeState']
+dwLocalPlayer = SIGNATURES['dwLocalPlayer']
+m_Collision = NETVARS['m_Collision']
+is_c4_owner = SIGNATURES['is_c4_owner']
+m_iCrosshairId = NETVARS['m_iCrosshairId']
+dwForceAttack = SIGNATURES['dwForceAttack']
+dwClientState = SIGNATURES['dwClientState']
+dwClientState_MaxPlayer = SIGNATURES['dwClientState_MaxPlayer']
+dwClientState_State = SIGNATURES['dwClientState_State']
