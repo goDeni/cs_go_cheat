bcdac89d93720c41f5e31e33744adbae15c2008f
goDeni <den4ikept11@gmail.com>
2020-05-10 13:46:46 +0500

    Добавил метод set_shoot_state

--- client_control.py
+++ client_control.py
@@ -1,9 +1,9 @@
-from ctypes.wintypes import DWORD
+from ctypes.wintypes import DWORD, BOOLEAN
 from typing import Any
 
 import offsets
 from player import Player
-from process import get_module_address, rpm
+from process import get_module_address, rpm, wpm
 
 CLIENT_MODULE_NAME = "client_panorama.dll"
 
@@ -21,6 +21,9 @@ class ClientControl:
     def _rpm(self, offset: int, c_type) -> Any:
         return rpm(self._handle, self._base_address + offset, c_type)
 
+    def _wpm(self, offset: int, data: bytes) -> bool:
+        return wpm(self._handle, self._base_address + offset, data)
+
     def get_local_player_pointer(self) -> int:
         return self._rpm(offsets.dwLocalPlayer, DWORD).value
 
@@ -34,3 +37,6 @@ class ClientControl:
     def get_player_by_index(self, index: int):
         player_pointer = self.get_player_pointer(index)
         return Player(self._handle, player_pointer)
+
+    def set_shoot_state(self, state: bool) -> bool:
+        return self._wpm(offsets.dwForceAttack, int.to_bytes(int(state), 1, 'big'))
