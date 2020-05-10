daf120352ee641fe68474f7f35b10e99e2d78765
goDeni <den4ikept11@gmail.com>
2020-05-10 11:42:39 +0500

    Добавил возможность указывать каллбеки на остановку игры

--- in_game_control.py
+++ in_game_control.py
@@ -10,6 +10,7 @@ class InGameControl(Thread):
     def __init__(self,
                  engine_control: EngineControl,
                  start_game_callback: Union[Callable[[Event], None], List[Callable[[Event], None]]] = lambda *args: None,
+                 stop_game_callback: Union[Callable[[Event], None], List[Callable[[Event], None]]] = lambda *args: None,
                  *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -20,20 +21,33 @@ class InGameControl(Thread):
         else:
             self._start_game_callbacks = start_game_callback
 
+        if not isinstance(stop_game_callback, list):
+            self._stop_game_callbacks = [stop_game_callback]
+        else:
+            self._stop_game_callbacks = stop_game_callback
+
         self._is_in_game = Event()
 
     def run(self) -> None:
         while not self._thread_stop_event.is_set():
             if self._is_in_game.is_set() and not self._engine_control.is_ingame():
                 self._is_in_game.clear()
+                for callback in self._stop_game_callbacks:
+                    callback()
+
                 play_signal(0.2, 400)
             elif not self._is_in_game.is_set() and self._engine_control.is_ingame():
                 self._is_in_game.set()
-                play_signal(0.2, 550)
                 for callback in self._start_game_callbacks:
                     callback(self._is_in_game)
+
+                play_signal(0.2, 550)
             time.sleep(1)
 
     def join(self, timeout: Optional[float] = 0) -> None:
         self._thread_stop_event.set()
+        self._is_in_game.clear()
+
+        for th in self._threads:
+            th.join(5)
         super().join(timeout)
